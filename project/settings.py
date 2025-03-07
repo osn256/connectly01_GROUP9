d2538ad2d80b57e0ceab784d79cb66dccc963bd6
osiasn@mmdc <124549042+osn256@users.noreply.github.com>
2025-03-07 20:46:02 +0800

    Update Pagination at settings.py

--- project/settings.py
+++ project/settings.py
@@ -70,13 +70,28 @@ SITE_ID = 1
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     ),
+    
     'DEFAULT_PERMISSION_CLASSES': (
         #'rest_framework.permissions.AllowAny',  # Allow any user to access the API
         'rest_framework.permissions.IsAuthenticated',  # Allow only authenticated users to access the API
     ),
-}
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+        'rest_framework.parsers.MultiPartParser',
+        'rest_framework.parsers.FormParser',
+    ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
+
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
